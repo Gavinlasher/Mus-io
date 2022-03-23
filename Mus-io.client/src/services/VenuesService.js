356e9c692fd8f8e30310c20e10f4235054acfca1
Katie McCauley <katiemccau02@gmail.com>
2022-03-23 17:08:42 -0600

    finished venue and band

--- Mus-io.client/src/services/VenuesService.js
+++ Mus-io.client/src/services/VenuesService.js
@@ -1,26 +1,29 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
 import { api } from "./AxiosService"
 
 class VenuesService {
-    async getAll(query = ""){
+    async getAll(query = "") {
         const res = await api.get('api/venues/' + query)
         AppState.venues = res.data
-        
+
     }
-    async createVenue(body){
+    async createVenue(body) {
         const res = await api.post('api/venues', body)
+        logger.log(res.data)
         AppState.activeVenue = res.data
         AppState.venues = [...AppState.venues, res.data]
     }
-    async getVenueById(id){
-        const res = await api.get('api/venues/'+id)
+    async getVenueById(id) {
+        const res = await api.get('api/venues/' + id)
         AppState.activeVenue = res.data
-        
+
     }
-    async deleteVenue(id){
+    async deleteVenue(id) {
         const res = await api.delete('api/venues/' + id)
         AppState.venues = AppState.venues.filter(b => b.id !== id)
     }
-    async editVenue(body, id){
+    async editVenue(body, id) {
         const res = await api.put('api/venues/' + id, body)
     }
 }
