bc1d154a4c16939fc6310580482157b0460754f3
Gage Lasher <rsgage21@gmail.com>
2022-03-23 14:07:11 -0600

    commit

---
+++ Mus-io.client/src/services/BandsService.js
@@ -0,0 +1,30 @@
+import { AppState } from "../AppState"
+
+class BandsService {
+    async getAll(query = ""){
+        const res = await api.get('api/bands/' + query)
+        AppState.bands = res.data
+        
+    }
+    async createBand(body){
+        const res = await api.post('api/bands', body)
+        AppState.activeBand = res.data
+        AppState.bands = [...AppState.bands, res.data]
+    }
+    async getBandById(id){
+        const res = await api.get('api/bands/'+id)
+        AppState.activeBand = res.data
+        
+    }
+    async deleteBand(id){
+        const res = await api.delete('api/bands/' + id)
+        AppState.bands = AppState.bands.filter(b => b.id !== id)
+    }
+    async editBand(body, id){
+        const res = await api.put('api/bands/' + id, body)
+    }
+
+}
+
+
+export const bandsService = new BandsService()
