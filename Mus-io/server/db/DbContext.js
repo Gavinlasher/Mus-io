082128d3f829306d19e88259d4e940dc8f5503ef
Justin Brower <justin.lee.brower@gmail.com>
2022-03-23 14:27:05 -0600

    comitt

--- Mus-io/server/db/DbContext.js
+++ Mus-io/server/db/DbContext.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { BandSchema } from '../models/Band';
 import { ValueSchema } from '../models/Value'
+import { VenueSchema } from '../models/Venue';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
@@ -8,7 +10,9 @@ class DbContext {
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
   Bands = mongoose.model('Band', BandSchema)
   Venues = mongoose.model('Venue', VenueSchema)
-  Offers = mongoose.model('Offer', OfferSchema)
+
+  // NOTE COMMENT THIS BACK IN WHEN WE MAKE THE SCHEMA
+  // Offers = mongoose.model('Offer', OfferSchema)
 }
 
 export const dbContext = new DbContext()
