a2f64401d320c3af15dc6f14502c6dbabcf5dca1
gavinlasher <Krgavin21@yahoo.com>
2022-03-23 16:06:26 -0600

    commit

--- Mus-io/server/models/Offer.js
+++ Mus-io/server/models/Offer.js
@@ -1,8 +1,36 @@
 import { Schema } from 'mongoose'
+import { BandSchema } from "./Band"
+import { VenueSchema } from "./Venue"
 
 export const OfferSchema = new Schema({
   venueId: { type: Schema.Types.ObjectId, required: true, ref: 'Venue' },
   bandId: { type: Schema.Types.ObjectId, required: true, ref: 'Band' },
   isAccepted: { type: Boolean, required: true, default: false },
-  status: { type: String, enum: ['pending', 'accepted', ''] }
+  status: { type: String, enum: ['pending', 'accepted', 'declind', 'deleted'] },
+  body: { type: String, required: true },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+}
+{ timestamps: true, toJSON: { virtuals: true } }
+)
+
+
+
+OfferSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
+
+BandSchema.virtual('band', {
+  localField: 'bandId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Band'
+})
+VenueSchema.virtual('venue', {
+  localField: 'venueId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Venue'
 })
