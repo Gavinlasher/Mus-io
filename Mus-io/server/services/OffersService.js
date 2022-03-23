594a376cce01b97bfe098680f7619bc95664ea0a
gavinlasher <Krgavin21@yahoo.com>
2022-03-23 16:40:45 -0600

    commit

--- Mus-io/server/services/OffersService.js
+++ Mus-io/server/services/OffersService.js
@@ -1,7 +1,10 @@
-
+import { dbContext } from '../db/DbContext'
 
 class OffersService {
-
+  async getAllMyOffers(query) {
+    const offers = await dbContext.Offers.find(query).populate('creator').populate('venue').populate('band')
+    return offers
+  }
 }
 
 export const offersService = new OffersService()
