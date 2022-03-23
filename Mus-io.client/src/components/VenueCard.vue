ad82743514904d19c70575ab2d9bccbb8940114c
Haven Fricke <robslaven@live.com>
2022-03-23 16:53:05 -0600

    haven venue card push

--- Mus-io.client/src/components/VenueCard.vue
+++ Mus-io.client/src/components/VenueCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="col-3 p-1 my-1 mx-1 bg-grey shadow hoverable rounded">
     <div class="col-12">
-      <h1 class="ms-2 p-1">Performer Name</h1>
-      <h5 class="ms-4">Genre</h5>
+      <h1 class="ms-2 p-1">Venue Name</h1>
+      <h5 class="ms-4">Location</h5>
       <img
         class="img-fluid rounded shadow mt-3"
         src="https://c.stocksy.com/a/7cL600/z9/1513055.jpg"
