5708020f260a5bd538d41bd1028dcc68d7901dd1
gavinlasher <Krgavin21@yahoo.com>
2022-03-23 14:17:12 -0600

    commit

---
+++ Mus-io/server/models/Band.js
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose'
+
+export const BandSchema = new Schema({
+  name: { type: String, required: true },
+  bannerImg: { type: String, required: true },
+  bio: { type: String, required: true },
+  soundCloud: { type: String, default: 'https://soundcloud.com/' },
+  spotify: { type: String, default: 'https://www.spotify.com/us/' },
+  basePrice: { type: Number, min: 0, required: true },
+  writer: { type: Array, required: true },
+  genre: { type: String, enum: ['rock', 'country', 'pop', 'rap', 'electronic'] },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+},
+  { timestamps: true, toJSON: { virtuals: true } }
+)
+
+BandSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
