27540039168d96bbff175f62efaac784a3489578
Gage Lasher <rsgage21@gmail.com>
2022-03-24 15:48:08 -0600

    commit

--- Mus-io.client/src/pages/BandPage.vue
+++ Mus-io.client/src/pages/BandPage.vue
@@ -28,6 +28,7 @@
                 <a :href="band.soundCloud"
                   ><h3><i class="mdi mdi-soundcloud"></i></h3>
                 </a>
+                
               </div>
               <!-- NOTE - Opens model to do offer -->
               <div class="col-12 ps-4 pb-3">
@@ -50,6 +51,8 @@
             <p class="ps-4 pb-3">
               {{ band.bio }}
             </p>
+            <iframe v-if="band.name == 'BLACKPINK'" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/277341570&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/genrse" title="Genna" target="_blank" style="color: #cccccc; text-decoration: none;">Genna</a> · <a href="https://soundcloud.com/genrse/blackpink-whistle" title="BLACKPINK - WHISTLE" target="_blank" style="color: #cccccc; text-decoration: none;">BLACKPINK - WHISTLE</a></div>
+            <iframe v-if="band.name == 'BTS'" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/605006934&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/l2sharebts2" title="L2ShareBTS♫2" target="_blank" style="color: #cccccc; text-decoration: none;">L2ShareBTS♫2</a> · <a href="https://soundcloud.com/l2sharebts2/boy-with-luv" title="Boy with Luv" target="_blank" style="color: #cccccc; text-decoration: none;">Boy with Luv</a></div>
           </div>
         </div>
       </div>
