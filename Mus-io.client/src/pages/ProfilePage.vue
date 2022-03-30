6e0a091fff6236d84eb70687adfb09ad5831ba10
gavinlasher <Krgavin21@yahoo.com>
2022-03-30 12:30:06 -0600

    commit

--- Mus-io.client/src/pages/ProfilePage.vue
+++ Mus-io.client/src/pages/ProfilePage.vue
@@ -215,6 +215,30 @@ export default {
         const sentOffer = AppState.offers.filter(v => v.status == 'accepted')
         return [...recOffer, ...sentOffer]
       }),
+      async goTo(id) {
+        try {
+          await bandsService.getBandById(id)
+          router.push({
+            name: 'Band',
+            params: { id: AppState.activeBand.id }
+          })
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error message')
+        }
+      },
+      async goTo2(id) {
+        try {
+          await venuesService.getVenueById(id)
+          router.push({
+            name: 'Venue',
+            params: { id: AppState.activeBand.id }
+          })
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error message')
+        }
+      }
 
     }
   }
