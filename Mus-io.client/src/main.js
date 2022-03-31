6f77268c557ba4917fd4efabfe8ad6cf156cee54
gavinlasher <Krgavin21@yahoo.com>
2022-03-31 12:02:30 -0600

    commit

--- Mus-io.client/src/main.js
+++ Mus-io.client/src/main.js
@@ -5,10 +5,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
+import FullCalendarComponent from "@fullcalendar/vue3"
+import FullCalendar from "@fullcalendar/vue3/dist/FullCalendar"
 
 const root = createApp(App)
 registerGlobalComponents(root)
 
 root
   .use(router)
+  .use(@FullCalendar)
   .mount('#app')
