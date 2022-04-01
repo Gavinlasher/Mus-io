7d82f859ea1819f8014142abbac251bf3cd240bf
Justin Brower <justin.lee.brower@gmail.com>
2022-04-01 16:25:13 -0600

    comitt

--- Mus-io.client/src/main.js
+++ Mus-io.client/src/main.js
@@ -5,8 +5,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
-import FullCalendarComponent from "@fullcalendar/vue3"
-import FullCalendar from "@fullcalendar/vue3/dist/FullCalendar"
+
 
 
 const root = createApp(App)
