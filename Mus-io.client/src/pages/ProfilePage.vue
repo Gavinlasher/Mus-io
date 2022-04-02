35a52f4b4fbd0a2e93672207e460a737ada92b40
Katie McCauley <katiemccau02@gmail.com>
2022-04-01 21:10:59 -0600

    fixed hoverable style issue

--- Mus-io.client/src/pages/ProfilePage.vue
+++ Mus-io.client/src/pages/ProfilePage.vue
@@ -74,7 +74,7 @@
     <h1 class="text-center text-light custom-text p-3" v-else>Performers</h1>
     <div class="row justify-content-center p-3 mt-5">
       <div
-        class="col-md-3 bg-dark p-0 m-3 shadow hoverable rounded"
+        class="col-md-3 bg-dark p-0 m-3 shadow rounded"
         v-for="b in myBands"
         :key="b.id"
       >
@@ -102,7 +102,7 @@
     </h1>
     <div class="row justify-content-center p-3 mt-5">
       <div
-        class="col-md-3 bg-dark p-0 m-3 shadow hoverable rounded"
+        class="col-md-3 bg-dark p-0 m-3 shadow rounded"
         v-for="v in myVenues"
         :key="v.id"
       >
